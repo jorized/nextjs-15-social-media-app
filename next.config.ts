a7a34f150647d235f185adc4ef1832bb37d503b9
jorized <e1138415@u.nus.edu>
2024-12-19 23:34:24 +0800

    Initial Project Setup Commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
-
+// This is to set up client-side caching for next JS. (For 30 seconds)
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    staleTimes: {
+      dynamic: 30
+    }
+  }
 };
 
 export default nextConfig;
